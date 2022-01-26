a9c4d4a8ea9b6e07a0df9781dc224c4ddf8ee227
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-01-26 16:36:43 +0300

    Tablo değişikliği yapıldı

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -10,7 +10,7 @@ namespace DataAccessLayer.Concrete
 {
     public class Context : DbContext
     {
-        //Deneeme
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-CCT1C3T;database=CoreDemoDb;integrated security=true;");
