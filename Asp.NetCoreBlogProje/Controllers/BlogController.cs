deeed12df9522a733c1d60e6e1b0c1992c6cd2d6
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-01-28 02:04:47 +0300

    Bloglar listelendi

--- Asp.NetCoreBlogProje/Controllers/BlogController.cs
+++ Asp.NetCoreBlogProje/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,11 @@ namespace CoreDemo.Controllers
 {
     public class BlogController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = bm.GetList();
+            return View(values);
         }
     }
 }
