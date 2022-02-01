6ae962479705ab78677189b0ed90d5787ece25f3
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-02-01 20:55:53 +0300

    Blog Devamını Oku sayfası yapıldı.

--- Asp.NetCoreBlogProje/Controllers/BlogController.cs
+++ Asp.NetCoreBlogProje/Controllers/BlogController.cs
@@ -16,9 +16,10 @@ namespace CoreDemo.Controllers
             var values = bm.GetBlogListWithCategory();
             return View(values);
         }
-        public IActionResult BlogDetails(int id)
+        public IActionResult BlogReadAll(int id)
         {
-            return View(); //dıarıdan aldıgı id ye gore işlemlerimizi gerçekleştirecek
+            var values = bm.GetBlogByID(id);
+            return View(values); //dışarıdan aldıgı id ye gore işlemlerimizi gerçekleştirecek
         }
     }
 }
