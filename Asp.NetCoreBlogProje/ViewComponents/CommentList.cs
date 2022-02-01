5f0874dade4480eb5826ea39b6597445d67efc89
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-02-02 01:40:16 +0300

    View Component eklendi

---
+++ Asp.NetCoreBlogProje/ViewComponents/CommentList.cs
@@ -0,0 +1,36 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    UserName="Feride"
+
+                },
+                new UserComment
+                {
+                    ID=2,
+                    UserName="Şeyma"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    UserName="Betül"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
