38ff885b06a019d0fac323900678d4f6919954ab
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-02-02 14:40:18 +0300

    Bloglara ait yorumlar listelendi

---
+++ Asp.NetCoreBlogProje/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+    public class CommentListByBlog:ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList(8);
+            return View(values);
+        }
+
+    }
+}
