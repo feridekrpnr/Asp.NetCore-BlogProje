02cc6c3d4ac8a1b9b5a0517faa30ec5ee0cef528
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-02-03 01:08:44 +0300

    Yazar Kaydı ve Sweet Alert kullanımı

--- Asp.NetCoreBlogProje/Controllers/RegisterController.cs
+++ Asp.NetCoreBlogProje/Controllers/RegisterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +11,20 @@ namespace CoreDemo.Controllers
 {
     public class RegisterController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index()
+        public IActionResult Index(Writer p)
         {
-
+            p.WriterStatus = true;
+            p.WriterAbout = "Deneme Test";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index", "Blog");
         }
     }
 }
