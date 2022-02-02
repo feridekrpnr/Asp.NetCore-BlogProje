6aaa8afa7d84102dd5db2938b6a5edd3c0607ec4
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-02-03 00:11:18 +0300

    Register işlemi ve Attribute

---
+++ Asp.NetCoreBlogProje/Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index()
+        {
+
+        }
+    }
+}
