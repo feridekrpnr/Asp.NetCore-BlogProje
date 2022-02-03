6277b0f5e3248c8cc167f770b8ce407ebf105f8f
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-02-03 15:21:08 +0300

    Fluent Validation

--- Asp.NetCoreBlogProje/Controllers/RegisterController.cs
+++ Asp.NetCoreBlogProje/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,10 +23,23 @@ namespace CoreDemo.Controllers
         [HttpPost]
         public IActionResult Index(Writer p)
         {
-            p.WriterStatus = true;
-            p.WriterAbout = "Deneme Test";
-            wm.WriterAdd(p);
-            return RedirectToAction("Index", "Blog");
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(p); //wv içindeki parametreden gelen değerleri kontrol et
+            if(results.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Deneme Test";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage); //Rule fordaki kurallara uyulmazsa
+                }
+            }
+            return View();
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{ 
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez"); //linkq->lambda komutu
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez.");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakterlik girişi yapın");
+        
+        }
+    }
+}
