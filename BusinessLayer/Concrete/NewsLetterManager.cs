5a05af902993cee26d4e958f2653f3d8e158adaf
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-02-04 17:21:39 +0300

    Mail Bülteni

---
+++ BusinessLayer/Concrete/NewsLetterManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class NewsLetterManager : INewsLetterService
+    {
+        INewsLetterDal _newsletterDal;
+
+        public NewsLetterManager(INewsLetterDal newsletterDal)
+        {
+            _newsletterDal = newsletterDal;
+        }
+
+        public void AddNewsLetter(NewsLetter newsLetter)
+        {
+            _newsletterDal.Insert(newsLetter);
+        }
+    }
+}
