f86a127463621964f8c09e7547762f376cf7868c
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-03-10 00:04:52 +0300

    Controllerde Authorize işlemi yapıldı.

---
+++ Asp.NetCoreBlogProje/Controllers/WriterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    [Authorize]
+    public class WriterController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+    }
+}
