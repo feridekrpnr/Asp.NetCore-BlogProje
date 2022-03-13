5ecdb6ad2bb755afc930d873aff4c3f100ec2de7
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-03-13 15:27:44 +0300

    Footer Last3Post

--- Asp.NetCoreBlogProje/Controllers/BlogController.cs
+++ Asp.NetCoreBlogProje/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
+    [AllowAnonymous]
     public class BlogController : Controller
     {
         BlogManager bm = new BlogManager(new EfBlogRepository());
@@ -22,5 +24,6 @@ namespace CoreDemo.Controllers
             var values = bm.GetBlogByID(id);
             return View(values); //dışarıdan aldıgı id ye gore işlemlerimizi gerçekleştirecek
         }
+
     }
 }
