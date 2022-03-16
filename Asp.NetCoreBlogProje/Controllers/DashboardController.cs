5d0c034e6d01e99a4512f0e851fd2ee7970354f2
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-03-16 19:28:37 +0300

    Dashboard İstatistikleri

--- Asp.NetCoreBlogProje/Controllers/DashboardController.cs
+++ Asp.NetCoreBlogProje/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,10 @@ namespace CoreDemo.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x => x.WriterID == 1).Count();
+            ViewBag.v3 = c.Categories.Count();
             return View();
         }
     }
